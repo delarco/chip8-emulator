869ae5c1e4e73c5abcecfe56d9654399fe6479d6
Leandro Del Arco <leandrodelarco@outlook.com>
2023-04-24 10:18:21 -0300

    Implements renderer draw

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,6 +7,8 @@ export class Renderer {
     displayHeight: number;
     pixelSize: number;
 
+    pixelData: ImageData;
+
     /**
      * Initialize renderer and clear the screen
      */
@@ -20,13 +22,19 @@ export class Renderer {
         this.displayHeight = displayHeight;
         this.pixelSize = pixelSize;
 
+        this.pixelData = this.context.createImageData(pixelSize, pixelSize);
+        this.pixelData.data[0]   = 255;
+        this.pixelData.data[1]   = 255;
+        this.pixelData.data[2]   = 255;
+        this.pixelData.data[3]   = 255;
+
         this.clear();
     }
     
     /**
      * Clear screen
      */
-    private clear(): void {
+    public clear(): void {
 
         this.context!.fillStyle = "rgb(50,50,50)";
         this.context!.fillRect(0, 0, this.displayWidth * this.pixelSize, this.displayHeight * this.pixelSize);
@@ -38,6 +46,17 @@ export class Renderer {
      */
     public draw(screen: Uint8Array): void {
 
-        // TODO: implement
+        this.clear();
+
+        for(let y = 0; y < this.displayHeight; y++) {
+
+            for(let x = 0; x < this.displayWidth; x++) {
+
+              if(screen[(y * this.displayWidth) + x] == 1) {
+                
+                this.context!.putImageData(this.pixelData, x * this.pixelSize, y *  this.pixelSize);
+              }
+            }
+        }
     }
 }
